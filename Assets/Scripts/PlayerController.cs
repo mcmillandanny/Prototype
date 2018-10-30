fa00f4bf0ef53f98a6a1da638628a2f1d3f88154
Dnny <mcmillandanny@gmail.com>
2018-10-30 15:14:24 -0400

    intial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //Jump Variables
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public float jumpPower;
+    float targetMoveSpeed;
+
+    //isGrounded
+    public bool isGrounded;
+    public LayerMask groundLayers;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //IsGrounded?
+         isGrounded = Physics2D.OverlapArea(new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f),
+              new Vector2(transform.position.x + 0.5f, transform.position.y - 0.51f), groundLayers);
+
+        //Movement of the player
+        targetMoveSpeed = Mathf.Lerp(rb.velocity.x, Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, Time.deltaTime * 10);
+        rb.velocity = new Vector2(targetMoveSpeed, rb.velocity.y);
+
+        // Jump
+        if (Input.GetKeyDown (KeyCode.Space) && isGrounded)  {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+	}
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+
+    IEnumerator RotateMe(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+        for (var t = 0f; t <= 1; t += Time.deltaTime / inTime)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            StartCoroutine(RotateMe(Vector3.forward * 90, 0.8f));
+        }
+        if (Input.GetKeyDown("q"))
+        {
+            StartCoroutine(RotateMe(Vector3.forward * -90, 0.8f));
+        }
+    }
+}
+
+
